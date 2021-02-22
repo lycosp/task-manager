a3aa2086e52c17221e4ce5a7117421fabe557d4e
lycosp <plycos@gmail.com>
2021-02-22 17:45:37 -0500

    fixed data loading on user-table

--- Application/Front-End/src/app/user-table/user-table.component.ts
+++ Application/Front-End/src/app/user-table/user-table.component.ts
@@ -1,50 +1,46 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { MatTable } from '@angular/material/table';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { UserTableDataSource } from './user-table-datasource';
 import { User } from '../../models/user.model';
 import { ConnectionService } from '../connection.service';
+import { DataSource } from '@angular/cdk/table';
 
 @Component({
   selector: 'app-user-table',
   templateUrl: './user-table.component.html',
-  styleUrls: ['./user-table.component.css']
+  styleUrls: ['./user-table.component.css'],
 })
 export class UserTableComponent implements AfterViewInit, OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatTable) table: MatTable<User>;
-  dataSource: UserTableDataSource;
+  dataSource: any;
   user: User;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['id', 'username', 'privilege'];
 
-  constructor(private connectionService: ConnectionService) {}
+  constructor(private connectionService: ConnectionService) { }
 
-  getUsers(): User[] {
+  getUsers(): void {
     let userData: User[] = [];
-    this.connectionService.getUsers().subscribe(results => {
+    this.connectionService.getUsers().subscribe((results) => {
       for (let i = 0; i < results.length; i++) {
-        userData[i] = this.user = {
-          id: results[i]['ID'],
-          username: results[i]['USERNAME'],
-          privilegeId: results[i]['PRIVILEGE_ID'],
-          privilege: results[i]['PRIVILEGE']
-        }
+        userData[i] = this.user;
       }
+      this.dataSource = new MatTableDataSource(userData);
+      setTimeout(() => {
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      }, 100);
     });
-    return userData;
   }
 
   ngOnInit() {
-    this.dataSource = new UserTableDataSource(this.getUsers());
+    this.getUsers();
   }
 
-  ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-    this.table.dataSource = this.dataSource;
-  }
+  ngAfterViewInit() { }
 }
